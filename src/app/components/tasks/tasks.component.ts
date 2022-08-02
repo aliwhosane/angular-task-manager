93152fa5660ab84eae5ce57b35210e9de757e937
Ali Husen <a.husen21@gmail.com>
2022-08-02 12:46:19 +0530

    using observable to display tasks

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { TASKS } from 'src/app/mock-tasks';
+
 import { Task } from 'src/app/TASK';
+import { TaskService } from 'src/app/services/task.service';
 
 @Component({
   selector: 'app-tasks',
@@ -8,10 +9,11 @@ import { Task } from 'src/app/TASK';
   styleUrls: ['./tasks.component.css']
 })
 export class TasksComponent implements OnInit {
-  tasks:Task[] = TASKS;
-  constructor() { }
+  tasks:Task[] = [];
+  constructor(private taskService: TaskService) { }
 
   ngOnInit(): void {
+    this.taskService.getTasks().subscribe(tasks => this.tasks = tasks)
   }
 
 }
---
+++ src/app/services/task.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { Task } from '../TASK';
+import { TASKS } from '../mock-tasks';
+import { Observable, of} from 'rxjs';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TaskService {
+  constructor() { }
+  getTasks():Observable<Task[]>{
+    const tasksObservable = of(TASKS);
+    return tasksObservable;
+  }
+}
