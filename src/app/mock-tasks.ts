b396e90edb35f80a52d4082f651ae333200d3c4a
Ali Husen <a.husen21@gmail.com>
2022-08-02 11:43:10 +0530

    display tasks from mock files

---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,28 @@
+import {Task} from './TASK';
+
+export const TASKS:Task[] = [
+    {
+        id:1,
+        text: 'Complete Merger',
+        day: 'Monday',
+        reminder: true
+    },
+    {
+        id:1,
+        text: 'Make Donna COO',
+        day: 'Yesterday',
+        reminder: true
+    },
+    {
+        id:1,
+        text: 'Make Louis Managing Partner',
+        day: 'Friday',
+        reminder: true
+    },
+    {
+        id:1,
+        text: 'Hire Mike',
+        day: 'Episode 1',
+        reminder: true
+    }
+]
